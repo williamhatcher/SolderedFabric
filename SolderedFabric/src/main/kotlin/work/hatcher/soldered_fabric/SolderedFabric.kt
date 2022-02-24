e46f0355c11d40ca07218332f214002c6bbf9f23
William Hatcher <william@hatcher.work>
2022-02-24 16:10:58 -0600

    Renamed Mod

--- SolderedFabric/src/main/kotlin/work/hatcher/soldered_fabric/SolderedFabric.kt
+++ SolderedFabric/src/main/kotlin/work/hatcher/soldered_fabric/SolderedFabric.kt
@@ -1,8 +1,8 @@
-package com.mod_author.mod_id
+package work.hatcher.soldered_fabric
 import net.fabricmc.api.ModInitializer
 @Suppress("UNUSED")
-object ModName: ModInitializer {
-    private const val MOD_ID = "mod_id"
+object SolderedFabric: ModInitializer {
+    private const val MOD_ID = "soldered_fabric"
     override fun onInitialize() {
         println("Example mod has been initialized.")
     }
